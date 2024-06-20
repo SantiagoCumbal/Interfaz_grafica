c12e32f0c2a8b4e0be055d2b10fa3b5d0629fa69
1-21-01-E043-09\POO <santiago.cumbalepn.edu.ec>
2024-06-20 09:57:24 -0500

    calculadora basica

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,8 +13,14 @@ public class Main {
         int numero1=Integer.parseInt(primernumero);
         int numero2=Integer.parseInt(segundonumero);
         int suma=numero1+numero2;
+        int resta=numero1-numero2;
+        int multiplicacion=numero1*numero2;
+        int division=numero1/numero2;
         //
         JOptionPane.showMessageDialog(null,"El resultado de la suma es: "+suma,"SUMA DE DOS ENTEROS",JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null,"El resultado de la resta es: "+resta,"SUMA DE DOS ENTEROS",JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null,"El resultado de la multiplicacion es: "+multiplicacion,"SUMA DE DOS ENTEROS",JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null,"El resultado de la division es: "+division,"SUMA DE DOS ENTEROS",JOptionPane.PLAIN_MESSAGE);
 
     }
 }
